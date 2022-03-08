4ae49bc2535f327a9e9a0245bce5ddb03c0a2acd
hacker41601 <monynichkiem@gmail.com>
2022-03-08 17:35:47 -0500

    normalizing, sigmoid function

--- nn.py
+++ nn.py
@@ -1,15 +1,44 @@
 #Author: Monynich Kiem
 #Date: 03/08/2022
-#Desc: CS460G Neural Network + Multilayer Perceptron
+#Desc: CS460G Multilayer Perceptrons
+#using MNIST dataset
+#each training example consists of csvs with 785 features, the first is a numeric label and the rest are image pixels
+#can have single or multiple output nodes
+#needs at least one hidden layer and nodes, the more layers and nodes: the more accurate
+#activation function is the sigmoid!
+#need to include accuracy
 
 import numpy as np
 import pandas as pd
 import math
 
+#hyperparameters
+alpha = .001
+epoch = 42
+hidden_layers = 1
+hidden_nodes = 2
+output_nodes = 1
+total_layers = hidden_layers + 1
+
+#activation function
 def sigmoid(exponent):
     sigmoid = 1 / (1 + np.exp(-exponent))
     return sigmoid
+#print(sigmoid(2)) #it works great moving on
 
-print(sigmoid(2)) #it works great moving on
+#reading in dataframes
+train01 = pd.read_csv('mnist_train_0_1.csv')
+test01 = pd.read_csv('mnist_test_0_1.csv')
+#train04 = pd.read_csv('mnist_train_0_4.csv')
+#test04 = pd.read_csv('mnist_test_0_4.csv')
+
+#normalize the features after column 1 b/c the first column is the labels
+def normalize(dataset):
+    norm_dataset = dataset.copy(deep = True)
+    norm_dataset[1:]/255
+    return norm_dataset
+
+print(normalize(train01))
+print(normalize(test01))
 
 
