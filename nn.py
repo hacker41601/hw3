edb1565dd839aad44ceeda2b3fc7f861e59f5f71
hacker41601 <monynichkiem@gmail.com>
2022-03-08 16:52:49 -0500

    create just baseline sigmoid function

--- nn.py
+++ nn.py
@@ -1,3 +1,15 @@
 #Author: Monynich Kiem
 #Date: 03/08/2022
 #Desc: CS460G Neural Network + Multilayer Perceptron
+
+import numpy as np
+import pandas as pd
+import math
+
+def sigmoid(exponent):
+    sigmoid = 1 / (1 + np.exp(-exponent))
+    return sigmoid
+
+print(sigmoid(2)) #it works great moving on
+
+
