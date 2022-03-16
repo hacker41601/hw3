c8e74954911c3475b21a6e380e924197e37bbf1c
hacker41601 <monynichkiem@gmail.com>
2022-03-16 19:23:14 -0400

    changed up format. may use definitions later

--- nn.py
+++ nn.py
@@ -7,108 +7,110 @@
 #needs at least one hidden layer and nodes, the more layers and nodes: the more accurate
 #activation function is the sigmoid!
 #need to include accuracy
+#Author: Monynich Kiem
+#Date: 03/08/2022
+#Desc: CS460G Multilayer Perceptrons
+#using MNIST dataset
+#each training example consists of csvs with 785 features, the first is a numeric label and the rest are image pixels
+#can have single or multiple output nodes
+#needs at least one hidden layer and nodes, the more layers and nodes: the more accurate
+#activation function is the sigmoid!
+#need to include accuracy
 
 import numpy as np
 import pandas as pd
-import math
+import difflib
 
 #hyperparameters
-alpha = .001
+alpha = .1
 epoch = 42
 
 #activation function
 def sigmoid(exponent):
-    sigmoid = 1 / (1 + np.exp(-exponent))
-    return sigmoid
+    return 1/(1 + np.exp(-exponent))
 #print(sigmoid(2)) #it works great moving on
 
-#reading in dataframes
-train01 = pd.read_csv('mnist_train_0_1.csv')
-test01 = pd.read_csv('mnist_test_0_1.csv')
-#train04 = pd.read_csv('mnist_train_0_4.csv')
-#test04 = pd.read_csv('mnist_test_0_4.csv')
-
-#print(train01)
-#print(train01.drop(train01.columns[[0]], axis = 1))
+#reading in dataframe
+train_data = pd.read_csv('mnist_train_0_1.csv', header = None)
+test_data = pd.read_csv('mnist_test_0_1.csv', header = None)
 
 #normalize the features after column 1 b/c the first column is the labels
 def normalize(dataset):
-    norm_dataset = dataset.copy(deep = True)
-    norm_dataset = norm_dataset.drop(norm_dataset.columns[[0]], axis = 1)
-    norm_dataset/255
-    return round(norm_dataset) #round to make it either 1 or 0
+    normalized = dataset/255
+    return normalized
 
-train_feat = normalize(train01)
-#print(train_feat)
-train_label = train01.iloc[:,0]
-#print(train_cat)
+#begin train:
+labels = []
+outputs = []
 
-test_feat = normalize(test01)
-#print(test_feat)
-test_label = test01.iloc[:,0]
-#print(test_cat)
+hidden_weights = []
+output_weights = []
 
-train_feat = np.array(train_feat)
-train_label = np.array(train_label)
+#785 features
+for i in range(785):
+    hidden_weights.append(list(np.random.uniform(-1,1,4)))
 
-test_feat = np.array(test_feat)
-test_label = np.array(test_label)
+#number of hidden nodes
+for i in range(4):
+    output_weights.append(np.random.uniform(-1,1))
 
-weights = []
-bias = []
+#take from linear regression proj
+for data in range(400):
+    input = train_data.iloc[data]
+    label = input[0]
+    labels.append(label)
+    input = input.to_numpy()
+    input[0] = 1
+    input = normalize(input)
+    curr_epoch = 0
+    while curr_epoch <= epoch:
+    #begin forward pass
+        hidden_input = np.dot(list(np.array(hidden_weights).transpose()), input)
+        hidden_output = list(map(sigmoid, hidden_input))
+        overall_output = sigmoid(np.dot(output_weights, hidden_output))
+        
+    #begin back prop
+        delta_output = (label - overall_output) * (overall_output) * (1 - overall_output)
+        delta_hidden = np.dot(delta_output, output_weights)
+        gradient_hidden = np.outer(delta_hidden, input)
+        gradient_hidden = np.transpose(gradient_hidden)
+        
+        temp_output = output_weights
+        for i in range(4):
+            output_weights[i] = temp_output[i] + alpha * hidden_output[i] * delta_output
+            
+        temp_hidden = hidden_weights
+        for i in range(785):
+            hidden_weights[i] = temp_hidden[i] + alpha * input[i] * gradient_hidden[i]
+        
+        overall_output = round(overall_output)
+        #print(overall_output)
+        curr_epoch += 1
+        if curr_epoch == epoch:
+            outputs.append(overall_output)
+    final_hidden = hidden_weights
+    final_output = output_weights
 
-#double check make sure this is correct
-for i in range(2):
-    if i == 0: #first layer aka input layer
-        weights.append(np.random.uniform(-1, 1, (len(test_feat[i]), 2)))
-        bias.append(np.random.uniform(-1, 1, (2,1)))
-    elif i != 1: #hidden layer
-        weights.append(np.random.uniform(-1, 1, (2, 2)))
-        bias.append(np.random.uniform(-1, 1, (2,1)))
-    else: #output aka last layer
-        weights.append(np.random.uniform(-1, 1, (2,1)))
-        bias.append(np.random.uniform(-1, 1, (1,1)))
+#begin test
+test_labels = []
+predictions = []
+for data in range(1000):
+    test_input = test_data.iloc[data]
+    test_label = test_input[0]
+    test_labels.append(test_label)
+    test_input = test_input.to_numpy()
+    test_input[0] = 1
+    test_input = normalize(test_input)
     
-    print(str(i))
-    print(type(bias[i]))
-    print(bias[i].shape)
-    print(weights[i].shape)
+    hidden_input = np.dot(list(np.array(final_hidden).transpose()), test_input)
+    hidden_output = list(map(sigmoid, hidden_input))
+    
+    overall_output = sigmoid(np.dot(final_output, hidden_output))
+    overall_output = round(overall_output)
+    
+    predictions.append(overall_output)
+    
+sm=difflib.SequenceMatcher(None, test_labels, predictions, autojunk = False)
+    
+print(sm.ratio()*100)
 
-for curr_epoch in range(epoch):
-    for i, ex in enumerate(train_feat): #enumerate loops over an interable object and keeps track of how many iterations have occured
-        #begin forward pass
-        features = train_feat[i]
-        #print(features.shape)
-        ground_truth = float(train_label[i])
-        #print(ground_truth)
-        output = []
-        for j in range(2):
-            if j == 0:
-                prod = np.transpose(weights[j]).dot(features)
-            else:
-                prod = np.transpose(weights[j]).dot(output[j-1])
-            input = prod + np.transpose(bias[j])
-            input = np.transpose(input)
-            input = np.array(input)
-            #print(input)
-            activated = sigmoid(input)
-            output.append(activated)
-            #print(output)
-            #print(output[j].shape)
-        output = float(output[-1])
-        #print(output)
-        raw_error = ground_truth - output
-        #print(raw_error)
-        #not sure if the logic is there will ask Thomas/Dr. Harrison
-        '''
-        #backprop stuff
-        deltas = []
-        for k in reversed(range(2)):
-            if k == 1:
-                delta = raw_error * (output * (1-output)) #scalar operation
-            else:
-                delta = weights[k+1].dot(deltas[0]) * (output[k] * (1-output[k]))
-                delta = np.array(delta)
-            deltas.insert(0, delta)
-            print(deltas[0])
-'''
